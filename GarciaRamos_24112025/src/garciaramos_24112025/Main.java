6500627e351137ddfad70f2b81ce3ad572114075
Leslie Michelle Garcia Ramos <ia.lramos@ufg.edu.sv>
2025-11-24 18:05:26 -0600

    Los grafos :D

---
+++ GarciaRamos_24112025/src/garciaramos_24112025/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package garciaramos_24112025;
+
+/**
+ *
+ * @author UFG
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GrafoRecursivo grafo = new GrafoRecursivo(4);
+        
+        grafo.insertarNodo('A'); //Indice 0
+        grafo.insertarNodo('B'); //Indice 1
+        grafo.insertarNodo('C'); //Indice 2
+        grafo.insertarNodo('D'); //Indice 3
+        
+        grafo.conectar(0, 1); //ARISTA (A, B)
+        grafo.conectar(0, 2); //ARISTA (A, C)
+        grafo.conectar(1, 3); //ARISTA (B, D)
+        grafo.conectar(2, 3); //ARISTA (C, D)
+        
+        System.out.println("Recorrido Depth First Search (DFS) desde el nodo A");
+        
+        grafo.dfs(0);
+        //TEORICAMENTE EL RECORRIDO ES:
+        //VISITA NODO A
+        //LUEGO VISITA EL NODO B (SERIA EL PRIMER ADYACENTE DE A)
+        //LUEGO VISITA D (QUE SERIA EL ADYACENTE DE B)
+        //LUEGO RETROCEDE(VUELVE A LA RAIZ) Y VISITA C(QUE SERIA EL ADYACENTE NO VISITADO DE A)
+    }
+    
+}
